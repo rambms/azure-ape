8ff86d63a8f2116e4647f5e5bc968a527e1aa037
rambms <rambms07@gmail.com>
2025-08-10 13:17:52 +0530

    first commit

---
+++ src/test/java/com/azure/ape/AzureApeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.ape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureApeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
